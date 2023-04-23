eb5e846ddfb26deb6b7b7f9de90e2a99cf38a86a
CryptoJoe33 <117700080+CryptoJoe33@users.noreply.github.com>
2023-04-23 15:32:40 -0600

    Final Files
    These are the final files for the queries. They don't look great but that's alright.

--- Reports/allProducts/allProducts.sql
+++ Reports/allProducts/allProducts.sql
@@ -1,5 +1,19 @@
-spool C:\CPRG250s\PrototypeGithub\PrototypeSystem_1\allProducts\allProducts.txt
+SET linesize 250
+SET pagesize 25
+SET feedback off
+SET echo OFF
+spool C:\CPRG250s\PrototypeGithub\PrototypeSystem_1\Reports\allProducts\allProducts.txt
 
+column "All Products" format a50
+COLUMN DUMMY NOPRINT
+COMPUTE SUM OF prod_price ON DUMMY
+BREAK ON DUMMY
+TTITLE CENTER 'All Products Available'
+BTITLE CENTER "Group 6"
+SELECT NULL DUMMY, prod_title, prod_description, prod_price
+FROM t_product
+WHERE prod_price >= 1
+ORDER BY prod_title;
+clear columns
 
-
-spool off
+spool OFF
