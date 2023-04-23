7323dbe77775274a2be8c1634bc94f332dd48727
Gabriel Leclerc <119021693+GabrielPJL12@users.noreply.github.com>
2023-04-23 14:08:59 -0600

    Delete template.sql

--- template.sql
+++
@@ -1,8 +0,0 @@
--- CPSY 250 F, Database Design and Programming
--- Project Deliverable 5: Prototype System
--- Joseph Adam, Zachary Larrive, Gabriel Leclerc, Gabriel Leclerc
--- 2023 / 04 / 21
-
--- Report Description: 
-
--- SQL:
