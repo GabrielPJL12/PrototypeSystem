70f233a7535dde5c6d518512fff1fa9819ff6233
Gabriel Leclerc <119021693+GabrielPJL12@users.noreply.github.com>
2023-04-22 21:02:52 -0600

    Create template.sql

---
+++ template.sql
@@ -0,0 +1,8 @@
+-- CPSY 250 F, Database Design and Programming
+-- Project Deliverable 5: Prototype System
+-- Joseph Adam, Zachary Larrive, Gabriel Leclerc, Gabriel Leclerc
+-- 2023 / 04 / 21
+
+-- Report Description: 
+
+-- SQL:
