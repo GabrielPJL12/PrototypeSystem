c66901b2fcb071929fd58a6da0881dca877d0f2f
Gabriel Leclerc <119021693+GabrielPJL12@users.noreply.github.com>
2023-04-23 13:10:42 -0600

    Update allSuppliers.sql

--- allSuppliers/allSuppliers.sql
+++ allSuppliers/allSuppliers.sql
@@ -1,20 +1,5 @@
 spool Z:\Code\PrototypeSystem\allSuppliers.txt
 
--- CPSY 250 F, Database Design and Programming
--- Project Deliverable 5: Prototype System
--- Gabriel Leclerc
--- 2023 / 04 / 21
-
--- Report Description: 
-
--- SUPPLIER REPORT
--- 2. For all suppliers
---   a) Total number of products
---   b) Total number of orders
---   c) Total value of all orders
-
--- SQL:
-
 column "Value of Orders" format a15
 SELECT SUM("Total Products") "All Products", SUM("Orders") "All Orders", TO_CHAR(SUM("Total Value"), '$999,990.00') "Value of Orders"
 FROM (SELECT supplier_name "Name", COUNT(product_num) "Total Products"
